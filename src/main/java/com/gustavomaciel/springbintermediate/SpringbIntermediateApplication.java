2c3a74ba781d809ab2925bc1df06a4abc636daa3
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-09-11 16:17:01 -0300

    First commit

---
+++ src/main/java/com/gustavomaciel/springbintermediate/SpringbIntermediateApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomaciel.springbintermediate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbIntermediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbIntermediateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavomaciel/springbintermediate/SpringbIntermediateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavomaciel.springbintermediate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbIntermediateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
