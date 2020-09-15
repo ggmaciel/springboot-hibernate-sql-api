b00e8c9b0a057d4ae0ed9a9789e79c88fe706dfc
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-09-15 13:36:50 -0300

    pedidos/{id} Endpoint

--- src/main/java/com/gustavomaciel/springbintermediate/repositories/ItemPedidoRepository.java
+++ src/main/java/com/gustavomaciel/springbintermediate/repositories/ItemPedidoRepository.java
@@ -3,7 +3,6 @@ package com.gustavomaciel.springbintermediate.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gustavomaciel.springbintermediate.domain.Categoria;
 import com.gustavomaciel.springbintermediate.domain.ItemPedido;
 
 @Repository
