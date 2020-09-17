48bc1977bb318019f63d51df375ecd3bbb9f8111
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-09-17 14:32:19 -0300

    Custom Cliente validation: unique email only

--- src/main/java/com/gustavomaciel/springbintermediate/repositories/ClienteRepository.java
+++ src/main/java/com/gustavomaciel/springbintermediate/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.gustavomaciel.springbintermediate.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gustavomaciel.springbintermediate.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/gustavomaciel/springbintermediate/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/gustavomaciel/springbintermediate/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gustavomaciel.springbintermediate.domain.Cliente;
 import com.gustavomaciel.springbintermediate.domain.enums.TipoCliente;
 import com.gustavomaciel.springbintermediate.dto.ClienteNewDTO;
+import com.gustavomaciel.springbintermediate.repositories.ClienteRepository;
 import com.gustavomaciel.springbintermediate.resources.exceptions.FieldMessage;
 import com.gustavomaciel.springbintermediate.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 
