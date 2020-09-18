122511682e25e1a7382c2da019ce7e2c624b71fb
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-09-18 16:04:02 -0300

    POST insert Pedido

---
+++ src/main/java/com/gustavomaciel/springbintermediate/services/BoletoService.java
@@ -0,0 +1,19 @@
+package com.gustavomaciel.springbintermediate.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.gustavomaciel.springbintermediate.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_WEEK, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
--- src/main/java/com/gustavomaciel/springbintermediate/services/PedidoService.java
+++ src/main/java/com/gustavomaciel/springbintermediate/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.gustavomaciel.springbintermediate.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gustavomaciel.springbintermediate.domain.ItemPedido;
+import com.gustavomaciel.springbintermediate.domain.PagamentoComBoleto;
 import com.gustavomaciel.springbintermediate.domain.Pedido;
+import com.gustavomaciel.springbintermediate.domain.enums.EstadoPagamento;
+import com.gustavomaciel.springbintermediate.repositories.ItemPedidoRepository;
+import com.gustavomaciel.springbintermediate.repositories.PagamentoRepository;
 import com.gustavomaciel.springbintermediate.repositories.PedidoRepository;
 import com.gustavomaciel.springbintermediate.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +22,44 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPrice());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
